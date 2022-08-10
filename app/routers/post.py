a113137c3c4a102932c209c0303093e4ea3856e4
Amit <amit.kumar@simply-jet.ch>
2022-08-10 14:53:51 +0530

    iniitial Commit

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,13 +12,13 @@ router = APIRouter(
 )
 
 
-@router.get('/',response_model=List[PostSchema])
+@router.get('/', response_model=List[PostSchema])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
 
 
-@router.post('/',response_model=PostSchema)
+@router.post('/', response_model=PostSchema)
 def create_post(post: PostSchema, db: Session = Depends(get_db)):
     new_post = models.Post(title=post.title, content=post.content)
     db.add(new_post)
@@ -31,7 +31,8 @@ def create_post(post: PostSchema, db: Session = Depends(get_db)):
 def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {id} not found")
     print(post)
     return post
 
@@ -41,11 +42,13 @@ def update_post(id: int, post: PostCreate, db: Session = Depends(get_db)):
 
     single_post_query = db.query(models.Post).filter(models.Post.id == id)
     update_post = single_post_query.first()
-    
-    if update_post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Can't update post with id: {id} not found")
 
-    update_post = single_post_query.update(post.dict(), synchronize_session=False)
+    if update_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Can't update post with id: {id} not found")
+
+    update_post = single_post_query.update(
+        post.dict(), synchronize_session=False)
 
     db.commit()
     return single_post_query.first()
@@ -56,8 +59,9 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     delete_post = db.query(models.Post).filter(models.Post.id == id)
 
     if delete_post.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Can't delete post with id: {id} not found")
-    
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Can't delete post with id: {id} not found")
+
     delete_post.delete(synchronize_session=False)
     db.commit()
     return {'message': "Post Deleted"}
